0f0a9874c7fd4067c13be07f30394dc79fb50c1c
daikiku10 <soccerdk19.910@gmail.com>
2022-04-25 22:14:12 +0900

    Suspenseについて

---
+++ react18-basics/src/components/SuspenseDemo.js
@@ -0,0 +1,47 @@
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+import { ExclamationCircleIcon } from '@heroicons/react/solid'
+import { Layout } from './Layout'
+import { Spinner } from './Spinner'
+import { FetchUsers } from './FetchUsers'
+import { FetchTasks } from './FetchTasks'
+import { FetchComments } from './FetchComments'
+
+export const SuspenseDemo = () => {
+  return (
+    <Layout>
+      <p className="mb-3 text-xl font-bold text-blue-500">Suspense Demo</p>
+      <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchUsers />
+          <FetchTasks />
+          <FetchComments />
+        </Suspense>
+      </ErrorBoundary>
+
+      {/* <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchTasks />
+        </Suspense>
+      </ErrorBoundary>
+
+      <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchComments />
+        </Suspense>
+      </ErrorBoundary> */}
+    </Layout>
+  )
+}
